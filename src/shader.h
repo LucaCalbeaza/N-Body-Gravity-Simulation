93bd2883526d93ce1a4d56d9f18c5244feef0203
Luca Calbeaza <lcalbeaza31@gmail.com>
2026-07-21 14:17:31 -0700

    Added shader.h
    shader.h is a documentation file for the Shader class

---
+++ src/shader.h
@@ -0,0 +1,41 @@
+/** 
+ * File: shader.h
+ * Description: Declarations for the Shader class. 
+*/
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+
+class Shader {
+public: 
+    // Shader Program ID
+    unsigned int ID;
+
+    /**
+     * Shader Constructor: Creates shader program from the 
+     * file path of the given vertex shader and fragment shader 
+     */
+    Shader(const char* vertexPath, const char* fragmentPath);
+
+    /**
+     * Active shader program
+     */
+    void use();
+
+    /**
+     * Set uniform variables for 4Vectors with type float, boolean or int 
+     */
+    void setVec4f(const std::string &name, float x, float y, float z, float w) const;
+    void setVec4b(const std::string &name, bool x, bool y, bool z, bool w) const;
+    void setVec4i(const std::string &name, int x, int y, int z, int w) const;
+
+    /**
+     * Destructor
+    */
+    void destroy();
+
+};
+
+#endif
