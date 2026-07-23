5a0ce13f7caa839d12847677769cc4c6a31731d6
Luca Calbeaza <lcalbeaza31@gmail.com>
2026-07-22 17:27:45 -0700

    Added Body Class
    Created body.h and body.cpp to represent the orbital objects

---
+++ src/body.cpp
@@ -0,0 +1,24 @@
+/** 
+ * File: body.cpp
+ * Description: Implementations for the Body class. 
+*/
+
+#include "body.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+
+Body::Body(glm::vec3 position, glm::vec3 velocity, glm::vec3 acceleration, float mass) {
+    this->position = position;
+    this->velocity = velocity;
+    this->acceleration = acceleration;
+    this->mass = mass;
+}
+
+void Body::update(float dt) {
+    velocity += acceleration * dt; 
+    position += velocity * dt;
+    acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
+}
+
---
+++ src/body.h
@@ -0,0 +1,35 @@
+/** 
+ * File: body.h
+ * Description: Declarations for the Body class. 
+*/
+
+#ifndef BODY_H
+#define BODY_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+
+class Body {
+public:
+    // Body Properties
+    glm::vec3 position;
+    glm::vec3 velocity;
+    glm::vec3 acceleration;
+    float mass;
+
+    /**
+     * Body Constructor: Creates a body with given position, velocity, 
+     * acceleration and mass  
+     */
+    Body(glm::vec3 position, glm::vec3 velocity, glm::vec3 acceleration, float mass);
+
+    /**
+     * Updates position and velocity over the given time interval dt 
+     */
+    void update(float dt);
+
+};
+
+#endif
