f76a096f1e3488162d74f6d128d5494e1d6d013e
Luca Calbeaza <lcalbeaza31@gmail.com>
2026-07-22 17:35:58 -0700

    Added simple N-Body simulation into main
    Implemented the first simple N-Body simulation running on 60FPS, 10 objects, and randomized initial positions.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
+#include <random>
+#include <cmath>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+
 #include "shader.h"
 #include "window.h"
 #include "mesh.h"
+#include "body.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -27,45 +31,82 @@ int main() {
     // Build the Shaders and the shader Program ---------------------
     Shader shader("src/shaders/vertexShader.txt", "src/shaders/fragmentShader.txt");
     
-    glm::vec3 positions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f), 
-        glm::vec3( 0.75f,  0.75f, 0.0f), 
-        glm::vec3(-0.75f, 0.75f, 0.0f),  
-        glm::vec3(-0.75f, -0.75f, 0.0f),  
-        glm::vec3( 0.75f, -0.75f, 0.0f)
-    };
-
-
+    // Create Circle Mesh
     std::vector<float> circleVertices;
     std::vector<unsigned int> circleIndices;
     Mesh mesh(circleVertices, circleIndices);
     mesh.createCircle(0.5f, 100, 1.0f, 0.0f, 0.0f);
-    
-    // ---------------------------------------
+
+    // Generate random star property data
+    std::random_device randomDevice;
+    std::mt19937 gen(randomDevice());
+    std::uniform_real_distribution<float> genRandom(-0.8f, 0.8f);
+
+    std::vector<Body> stars;
+    unsigned int n = 10;
+    float m = 1.0f;
+    const float G = 0.1f;
+    float minDistance = 0.05f;
+    for (int i = 0; i < n; i++) {
+        glm::vec3 position = glm::vec3(genRandom(gen),  genRandom(gen),  0);
+        glm::vec3 veloctiy = glm::vec3(0.1*genRandom(gen),  0.0f,  0.0f);
+        glm::vec3 acceleration = glm::vec3(0.0f,  0.0f,  0.0f);
+        Body star(position, veloctiy, acceleration, m/n);
+        stars.push_back(star);
+    }
+
+    // Time Variables : 60 FPS Update Rate
+    const float dt = 1.0 / 60.0; 
+    float frameTimeAccumulation = 0.0;
+    float lastFrameTime = glfwGetTime();
+        
 
     // While loop runs while the window remains open
     while(!glfwWindowShouldClose(window.window)) {
         // Register Input
         window.processInput();
 
-        // Render
+        // Frame time calculation
+        float currentFrameTime = glfwGetTime();
+        float frameTime = currentFrameTime - lastFrameTime;
+        lastFrameTime = currentFrameTime;
+
+        if (frameTime > 0.25) {
+            frameTime = 0.25;
+        }
+        frameTimeAccumulation += frameTime;
+
+        while (frameTimeAccumulation >= dt) {
+            for (int a = 0; a < stars.size(); a++) {
+                for (int b = a + 1; b < stars.size(); b++) {
+                    if (a != b) {
+                        glm::vec3 distance = stars[a].position - stars[b].position;
+                        stars[a].acceleration -= ((G * stars[b].mass * glm::normalize(distance)) / (float)pow(glm::length(distance) + minDistance, 2));
+                        stars[b].acceleration += ((G * stars[a].mass * glm::normalize(distance)) / (float)pow(glm::length(distance) + minDistance, 2));
+                    }
+                }
+            }
+
+            for (auto& star : stars) {
+                star.update(dt);
+            }
+            frameTimeAccumulation -= dt;
+        }
+
+        // Render and active shader program
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // Draw -----
-        // Activate Program
         shader.use();
 
         // Bind VAO and Draw
-        for(unsigned int i = 0; i < 5; i++) {
+        for(unsigned int i = 0; i < n; i++) {
             glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, positions[i]);
+            model = glm::translate(model, stars[i].position);
             float angle = 20.0f * i; 
             model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
-            model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+            model = glm::scale(model, glm::vec3(0.05, 0.05, 0.05));
             mesh.draw(model, shader);
         }
-        // ----------
 
         // Swap buffers and poll for events
         window.update();    
