f0b73a7e422fb63635fa49ad04e4785d72f9d719
Luca Calbeaza <lcalbeaza31@gmail.com>
2026-08-15 15:25:39 -0700

    Added dear imGUI source files to project
    Added imGUI source files to a seperate folder within /include directory and then added the include calls in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "simulation.h"
 #include "graphics/window.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 
 // Main Class
 int main() {
     Window window(1000, 1000, "N-Body Orbital Simulation", true);
-    Simulation simulation(window, 60.0f, 50000, 1.0f, 0.1f, 0.9f, false);
+    Simulation simulation(window, 60.0f, 100000, 1.0f, 0.1f, 1.0f, true);
     return 0;
 }
